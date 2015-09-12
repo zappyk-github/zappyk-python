5d3d57a669ea112f14bc6ac05135b3c43fa2a4af
Carlo Zappacosta <zappyk@gmail.com>
2015-09-12 17:33:17 +0200

    Miglioire...

--- src/src_zappyk/googleSheets/GoogleSheets.sh
+++ src/src_zappyk/googleSheets/GoogleSheets.sh
@@ -6,10 +6,12 @@ PRG_VERS='0.0.2b1'
 HOSTARCH='32bit_ELF'
 HOSTARCH='64bit_ELF'
 HOSTARCH="$(getconf LONG_BIT)bit_ELF"
+PYTHON_V="3.4.2"
+PYTHON_V=($(python3 -V))
 
 DIR_NAME=$(dirname "$0")
 DIR_BASE="$PRG_NAME-$PRG_VERS"
-DIR_ARCH="exe.linux-$HOSTARCH-python-3.4.2"
+DIR_ARCH="exe.linux-$HOSTARCH-python-${PYTHON_V[1]}"
 
 PRG_BASE="$DIR_NAME/$DIR_BASE"
 PRG_EXEC="$DIR_ARCH/$PRG_NAME"

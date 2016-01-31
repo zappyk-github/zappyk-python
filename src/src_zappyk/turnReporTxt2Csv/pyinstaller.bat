f3e2229de90edd3917d61cb389313cdb51dd106b
Carlo Zappacosta <zappyk@gmail.com>
2016-02-01 00:36:59 +0100

    Miglioire...

--- src/src_zappyk/turnReporTxt2Csv/pyinstaller.bat
+++ src/src_zappyk/turnReporTxt2Csv/pyinstaller.bat
@@ -2,6 +2,17 @@
 call winenv.bat
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-pyinstaller.exe ^
- --onefile --windowed --icon=images\gear.ico --version-file=pyinstaller-version.txt ^
- TurnReporTxt2Csv\main.py -rg
+set pyinstaller=pyinstaller.exe --onefile --windowed --icon=images\gear.ico --version-file=pyinstaller-version.txt
+set pyinstaller=pyinstaller.exe --onefile --windowed --icon=images\gear.ico
+set pyinstaller=pyinstaller.exe --onefile --windowed --log-level=DEBUG --clean
+
+rem pyinstaller --noconfirm --log-level=WARN ^
+rem     --onefile --nowindow ^
+rem     --hidden-import=secret1 ^
+rem     --hidden-import=secret2 ^
+rem     --icon-file=..\MLNMFLCN.ICO ^
+rem     myscript.spec
+
+rem %pyinstaller% TurnReporTxt2Csv\main.py -rg
+
+TurnReporTxt2Csv\main.py -rg

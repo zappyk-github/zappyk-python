aee343b7df0bccacd03ad57f7e645011655feaf3
Carlo Zappacosta <zappyk@gmail.com>
2017-02-17 16:54:33 +0100

    Miglioire...

--- src/src_zappyk/developing/test-zi-url-4-selenium.py
+++ src/src_zappyk/developing/test-zi-url-4-selenium.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from pydoc import browse
-
 __author__ = 'zappyk'
 
 import sys, re, time
@@ -9,24 +7,31 @@ import sys, re, time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#from pydoc import browse
+
 browser_wd = 'Firefox'
-#browser_wd = 'PhantomJS'
+browser_wd = 'PhantomJS'
 #browser_wd = 'Chrome'
 
 browser_wd_PhantomJS_screenshot = 'screenshot.png'
-browser_wd_PhantomJS_htmlString = '<html><body><div style="background-color:red;height:500px;width:500px;">This is a png</div></body></html>'
+browser_wd_PhantomJS_screenshot = 'screenshot.jpg'
+browser_wd_PhantomJS_htmlstring = '<html><body><div style="background-color:red;height:500px;width:500px;">This is a png</div></body></html>'
 
-user_agent = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'
+user_agent = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1"
 base_path_ = "https://test.payroll.it/HRPW"
-base_path_ = "https://saas.hrzucchetti.it/hrppbs"
+#base_path_ = "https://saas.hrzucchetti.it/hrppbs"
 page_home_ = base_path_ + "/HRPW/jsp/home.jsp"
 page_login = base_path_ + "/jsp/login.jsp"
 page_query = base_path_ + "/servlet/cost_bexecrunsql?m_cWindowName=main"
 username_v = 'administrator'
 password_v = '!S3rv1c3s!'
 password_v = '!4cc4d3m14!'
-username_t = "m_cUserName"
-password_t = "m_cPassword"
+username_t = 'm_cUserName'
+password_t = 'm_cPassword'
 
 type_login = "//*[@type='submit']"
 exec_query = ".btsalva"              #    => find_element_by_css_selector(exec_query)
@@ -99,15 +104,34 @@ def find_element_by_regex(bw=None, xpath='id', regex=None, debug=0):
     return(xpa)
 
 ########################################################################################################################
+def _browser_is_Firefox(browser_webdrive):
+    if browser_webdrive == 'Firefox':
+        return(True)
+    return(False)
+########################################################################################################################
+def _browser_is_PhantomJS(browser_webdrive):
+    if browser_webdrive == 'PhantomJS':
+        return(True)
+    return(False)
+########################################################################################################################
+def _browser_is_Chrome(browser_webdrive):
+    if browser_webdrive == 'Chrome':
+        return(True)
+    return(False)
+########################################################################################################################
 def _browser(browser_webdrive):
     browser = None
-    if browser_webdrive == 'Firefox':
+    if _browser_is_Firefox(browser_webdrive):
         browser = webdriver.Firefox()
-    if browser_webdrive == 'PhantomJS':
-        browser = webdriver.PhantomJS()
+    if _browser_is_PhantomJS(browser_webdrive):
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.resourceTimeout"] = ("10000")
+    #CZ#browser = webdriver.PhantomJS()
+        browser = webdriver.PhantomJS(desired_capabilities=dcap, service_args=['--ignore-ssl-errors=true'])
         browser.set_window_size(1920, 1080)
-        browser.get_screenshot_as_file(browser_wd_PhantomJS_screenshot)
-    if browser_webdrive == 'Chrome':
+        browser.implicitly_wait(30)
+        #browser.get_screenshot_as_file(browser_wd_PhantomJS_screenshot)
+    if _browser_is_Chrome(browser_webdrive):
         browser = webdriver.Chrome()
     if browser is None:
         _writeln('Browser %s not implemented :-(' % browser_webdrive)
@@ -123,42 +147,64 @@ browser.get(page_login)
 _sleep(3)
 _writeln()
 
+#content = browser.save_screenshot('login.jpg')
+browser.save_screenshot('login.jpg')
+
 _write("Login credential:")
 username = browser.find_element_by_name(username_t)
 password = browser.find_element_by_name(password_t)
-username.send_keys(username_v)
+if _browser_is_PhantomJS(browser_wd):
+    wait = WebDriverWait(browser, 10)
+    password = wait.until(EC.visibility_of_element_located((By.NAME, password_t)))
+    #password.send_keys(password_v)
+username.clear()
+password.send_keys(username_v)
+username.clear()
 password.send_keys(password_v)
 login_attempt = browser.find_element_by_xpath(type_login)
 login_attempt.click()
-_sleep(3)
+_sleep(10)
 _writeln()
 
-_write("Open Page query:")
+#if _browser_is_PhantomJS(browser_wd):
+#    #browser.execute_script("window.alert = function(){}")
+#    browser.execute_script("window.confirm = function(msg) { return true; }")
+
+_write("Open page query:")
 browser.get(page_query)
 _sleep(3)
 _writeln()
 
-#browser.execute_script("document.write('{}');".format(browser_wd_PhantomJS_htmlString))
-#browser.save_screenshot(browser_wd_PhantomJS_screenshot)
+browser.save_screenshot('home.jpg')
+sys.exit(0)
 
-#try:
-##CZ#browser.save_screenshot('screenshot.png')
-##CZ#browser.get_screenshot_as_file('screenshot.png')
-#    print('########################################################################################################################')
-#    print(browser.page_source())
-#    print('########################################################################################################################')
-#except Exception as e:
-#    print(e)
+# if _browser_is_PhantomJS(browser_wd):
+#     browser.execute_script("document.write('{}');".format(browser_wd_PhantomJS_htmlstring))
+#     browser.save_screenshot(browser_wd_PhantomJS_screenshot)
+#     try:
+#     #CZ#browser.save_screenshot('screenshot.png')
+#     #CZ#browser.get_screenshot_as_file('screenshot.png')
+#         print('########################################################################################################################')
+#         print(browser.page_source)
+#         print('########################################################################################################################')
+#     except Exception as e:
+#         print(e)
 
 _write("Page alert accept:")
-if browser_wd == 'Firefox'\
-or browser_wd == 'Chrome':
+if _browser_is_Firefox(browser_wd)\
+or _browser_is_Chrome(browser_wd):
 #CZ#browser.switch_to_alert().accept()
     browser.switch_to.alert.accept()
-if browser_wd == 'PhantomJS':
-    browser.execute_script("window.confirm = function(msg) { return true; }")
+if _browser_is_PhantomJS(browser_wd):
+    #browser.execute_script("window.confirm = function(msg) { return true; }")
+    #browser.switch_to.alert.accept()
+    pass
 _sleep(3)
 _writeln()
+
+print('########################################################################################################################')
+print(browser.page_source)
+print('########################################################################################################################')
 
 _writeln("\t|\n\t| here is Insert & Execute query\n\t|")
 
@@ -176,7 +222,7 @@ sql_exec.click()
 _sleep(9)
 _writeln()
 
-_write("Result query:")
+_writeln("Result query:")
 ids_resu = find_element_by_regex(browser, 'id', exec_resul)
 browser.switch_to.frame(ids_resu[0])
 #print(browser.page_source)
@@ -190,7 +236,7 @@ _writeln()
 
 #_read("<press key to continue>")
 
-_write("Back Page:")
+_write("Back page:")
 #browser.execute_script("window.history.go(-1)")
 #browser.execute_script("window.history.go(-2)")
 browser.back()
@@ -207,8 +253,8 @@ _sleep(3)
 _writeln()
 
 _writeln("Close browser %s." % browser_wd)
-#browser.close()
-browser.quit()
+#browser.quit()
+browser.close()
 _writeln("Success :-)")
 
 sys.exit(0)

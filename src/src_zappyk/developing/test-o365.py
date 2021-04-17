9b49fc1f550ef16d31d33dc4c9888e92e8b26e83
Carlo Zappacosta <zappyk@gmail.com>
2021-04-17 21:54:41 +0200

    Continua svoluppo con libreria O365.

--- src/src_zappyk/developing/test-o365.py
+++ src/src_zappyk/developing/test-o365.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 __author__ = 'zappyk'
 ########################################################################################################################
-from O365 import Account
+import os
+
+from O365 import Account, FileSystemTokenBackend
 
 auth_flow_CREDENTIALS = 'credentials'
 auth_flow_AUTHORIZATION = 'authorization'
 
 tenant_id = '8880a91d-ce48-4314-9921-359b8a3d83a3' # P&S Payroll Services
 
+token_backend = FileSystemTokenBackend(token_path=os.getcwd()+'/resources/test-o365.tokens/', token_filename='o365_token.my.txt')
 #credentials = ('e71c20d5-7cae-4cda-aec7-63439e7dea80', 'ejdUTX9564~[)ttncLYBB2$') # Webexe connector
 credentials = ('f2313123-255b-4d30-bd85-77529b9d887d', 'ZAtz1zzhK-0-nGSr.CDD5qBOy1-82OcU43') # PeS connector, expire 17/04/2023
 
@@ -20,8 +23,9 @@ if (auth_flow_s == auth_flow_CREDENTIALS):
     scopes_list = None
 if (auth_flow_s == auth_flow_AUTHORIZATION):
     scopes_list = ['basic', 'message_all']
+    scopes_list = ['message_all']
 
-account = Account(credentials, tenant_id=tenant_id, auth_flow_type=auth_flow_s)
+account = Account(credentials, tenant_id=tenant_id, auth_flow_type=auth_flow_s, token_backend=token_backend)
 account.authenticate(scopes=scopes_list)
 print("Authenticate is %s" % account.is_authenticated)
 #exit(1)
